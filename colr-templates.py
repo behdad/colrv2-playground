64f125fea800615ecdae8b5fd8fea1c0387b423d
Behdad Esfahbod <behdad@behdad.org>
2023-08-02 15:31:02 -0600

    Second try

--- colr-templates.py
+++ colr-templates.py
@@ -2,6 +2,8 @@ from fontTools.ttLib import TTFont
 from fontTools.ttLib.tables.otTables import Paint
 from collections import defaultdict
 from pprint import pprint
+import sys
+
 
 font = TTFont("NotoColorEmoji-Regular.ttf")
 colr = font["COLR"].table
@@ -10,7 +12,14 @@ glyphList = colr.BaseGlyphList.BaseGlyphPaintRecord
 layerList = colr.LayerList.Paint
 
 
-def objectTupleForObject(obj):
+def objectToTuple(obj):
+    if isinstance(obj, (int, float, str)):
+        return obj
+    if isinstance(obj, (list, tuple)):
+        return (type(obj).__name__,) + tuple(objectToTuple(o) for o in obj)
+
+    return (type(obj).__name__,) + tuple((attr, objectToTuple(getattr(obj, attr))) for attr in sorted(obj.__dict__.keys()))
+
 
 def templateForObjectTuple(objTuple):
     if not isinstance(objTuple, tuple):
@@ -19,65 +28,35 @@ def templateForObjectTuple(objTuple):
     if objTuple[0] == 'unique.Paint':
         return 'PaintVar'
 
-    template = tuple(templateForObjectTuple(o) for o in objTuple)
+    if objTuple[0] == 'Paint':
+        if not any(isinstance(o, tuple) and o[0] == 'Paint' for o in objTuple[1:]):
+            # Leaf paint. Replace with variable.
+            return ('PaintArgument',)
 
-    return template
+    return tuple(templateForObjectTuple(o) for o in objTuple)
 
 
-def unifyObject(obj, uniqueObjects, uniqueObjectsInverse, layerList):
-    tp = 'unique.' + type(obj).__name__
-    if id(obj) in uniqueObjects:
-        return (tp, id(obj))
-
-    if isinstance(obj, Paint) and obj.Format == 1:  # ObjectColrLayers:
-        obj = tuple(
-            unifyObject(p, uniqueObjects, uniqueObjectsInverse, layerList)
-            for p in layerList[obj.FirstLayerIndex:obj.FirstLayerIndex+obj.NumLayers]
-        )
-    elif isinstance(obj, list):
-        if obj and not isinstance(obj[0], (int, float, str)):
-            obj = tuple(tuple(sorted(o.__dict__.items())) for o in obj)
-        else:
-            obj = tuple(obj)
-    else:
-        for attr in list(obj.__dict__.keys()):
-            value = getattr(obj, attr)
-            if isinstance(value, (int, float, str, tuple)):
-                continue
-            setattr(obj, attr, unifyObject(value, uniqueObjects, uniqueObjectsInverse, layerList))
-
-    if isinstance(obj, tuple):
-        tupleObject = obj
-    else:
-        tupleObject = tuple(sorted(obj.__dict__.items()))
-
-    if tupleObject in uniqueObjectsInverse:
-        return (tp, uniqueObjectsInverse[tupleObject])
-
-    uniqueObjects[id(obj)] = obj
-    uniqueObjectsInverse[tupleObject] = id(obj)
-    return (tp, id(obj))
-
-uniqueObjects = {} # From unique paint object ID to paint
-uniqueObjectsInverse = {} # From paint tuple to unique paint object ID
-basePaints = {}
+uniqueTemplates = defaultdict(list)
 for glyph in glyphList:
     glyphName = glyph.BaseGlyph
     paint = glyph.Paint
-    paint = unifyObject(paint, uniqueObjects, uniqueObjectsInverse, layerList)
-    basePaints[glyphName] = paint
 
-print(len(basePaints), "root paints")
-print(len(uniqueObjects), "unique objects")
+    if paint.Format == 1:  # ObjectColrLayers:
+        paint = [p for p in layerList[paint.FirstLayerIndex:paint.FirstLayerIndex+paint.NumLayers]]
 
-templateCounts = defaultdict(int)
-for objTuple in uniqueObjectsInverse:
-    template = templateForObjectTuple(objTuple)
+    paintTuple = objectToTuple(paint)
+    paintTemplate = templateForObjectTuple(paintTuple)
 
-    if template == objTuple: continue
-    if all(o == 'PaintVar' for o in template): continue
+    if paintTemplate[0] == 'PaintArgument':
+        continue
+    if (paintTemplate[0] == 'list' and
+        all(o == ('PaintArgument',) for o in paintTemplate[1:])):
+        continue
 
-    templateCounts[template] += 1
+    uniqueTemplates[paintTemplate].append(glyphName)
 
-print(len(templateCounts), "unique templates")
-pprint([(v, k) for k,v in sorted(templateCounts.items(), key=lambda x: x[1]) if v > 1])
+uniqueTemplates = {k:v for k,v in uniqueTemplates.items() if len(v) > 1}
+
+print(len(glyphList), "root paints")
+print(len(uniqueTemplates), "unique templates")
+pprint([(len(v), v, k) for k,v in sorted(uniqueTemplates.items(), key=lambda x: len(x[1]))])
